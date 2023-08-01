b497d95ae7c8e804d4a9961ea35f274d97b0a157
horacio <114161010+asalvz@users.noreply.github.com>
2023-08-01 18:14:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
+// Conexión con MetaMask y eventos
 let web3;
 let userAddress;
-
-
 
 // Comprobar si web3 está disponible en el navegador
 if (typeof window.ethereum !== 'undefined') {
@@ -17,7 +16,7 @@ async function connectToMetaMask() {
 
     // Obtener la dirección del usuario conectado
     const accounts = await web3.eth.getAccounts();
-    userAddress = accounts[0]; // Quitamos la palabra clave 'const'
+    userAddress = accounts[0];
     console.log('Dirección del usuario:', userAddress);
 
     // Obtener el saldo del usuario conectado en BNB
@@ -34,7 +33,6 @@ async function connectToMetaMask() {
   }
 }
 
-
 // Función para obtener el saldo del usuario en BNB
 async function getBalance(address) {
   const weiBalance = await web3.eth.getBalance(address);
@@ -42,182 +40,39 @@ async function getBalance(address) {
   return parseFloat(balance).toFixed(4);
 }
 
-
-
-
-const elems = document.querySelectorAll('.laya-please');
-const layer2 = document.querySelector('.layer-2');
-const layer3 = document.querySelector('.layer-3');
-const layer4 = document.querySelector('.layer-4');
-const layer5 = document.querySelector('.layer-5');
-const layer6 = document.querySelector('.layer-6');
-const layer7 = document.querySelector('.layer-7');
-const layer8 = document.querySelector('.layer-8');
-
-
-setTimeout(function () {
-    elems.forEach(function (elem, index) {
-        elem.style.animation = "none";
-    });
-}, 1500);
-
-
-
-document.body.addEventListener('mousemove', function (e) {
-    if (!e.currentTarget.dataset.triggered) {
-        elems.forEach(function (elem, index) {
-            if (elem.getAttribute('style')) {
-                elem.style.transition = "all .5s";
-                elem.style.transform = "none";
-            }
-        });
-    }
-    e.currentTarget.dataset.triggered = true;
-    
-    let width = window.innerWidth / 2;
-    let mouseMoved2 = ((width - e.pageX) / 50);
-    let mouseMoved3 = ((width - e.pageX) / 40);
-    let mouseMoved4 = ((width - e.pageX) / 30);
-    let mouseMoved5 = ((width - e.pageX) / 20);
-    let mouseMoved6 = ((width - e.pageX) / 10);
-    let mouseMoved7 = ((width - e.pageX) / 5);
-
-    layer3.style.transform = "translateX(" + mouseMoved2 + "px)";
-    layer4.style.transform = "translateX(" + mouseMoved3 + "px)";
-    layer5.style.transform = "translateX(" + mouseMoved4 + "px)";
-    layer6.style.transform = "translateX(" + mouseMoved5 + "px)";
-    layer7.style.transform = "translateX(" + mouseMoved6 + "px)";
-    layer8.style.transform = "translateX(" + mouseMoved7 + "px)";
-});
-
-document.body.addEventListener('mouseleave', function (e) {
-    elems.forEach(function (elem, index) {
-        elem.style.transition = "all .5s";
-        elem.style.transform = "none";
-    });
-});
-
-document.body.addEventListener('mouseenter', function (e) {
-    elems.forEach(function (elem, index) {
-        setTimeout(function () {
-            elem.style.transition = "none";
-        }, 500);
-    });
-});
-
-
-
- function generateReferralLink() {
-    if (!userAddress) {
-      alert('Conéctate a MetaMask para obtener la dirección de tu billetera.');
-      return;
-    }
-
-    // Generar el enlace de referido con la dirección como parámetro
-    const referralLink = `https://tusitio.com/registro?ref=${userAddress}`;
-
-    // Abreviar el enlace de referido para mostrar solo los primeros 10 caracteres
-    const shortenedLink = `${referralLink.substring(0, 30)}...`;
-
-    // Actualizar el contenido del elemento con el enlace abreviado
-    const linkElement = document.getElementById('link');
-    linkElement.textContent = shortenedLink;
-
-    // Cambiar el texto y el evento onclick del botón para que ahora copie el enlace abreviado
-    const generateButton = document.getElementById('generate-referral-button');
-    generateButton.textContent = 'Copiar enlace';
-    generateButton.onclick = function() {
-      copyToClipboard(referralLink);
-      alert('Enlace copiado al portapapeles: ' + referralLink);
-    };
-  }
-
-  function copyToClipboard(text) {
-    const tempTextArea = document.createElement('textarea');
-    tempTextArea.value = text;
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempTextArea);
-  }
-// Función para mostrar el banner una vez cargada la página
-$(document).ready(function() {
-    // Ocultar el banner al inicio
-    $("#banner-dialog").hide();
-
-    // Comprobar si todos los elementos de la página están cargados
-    $(window).on("load", function() {
-        // Mostrar el banner una vez que todo esté cargado
-        $("#banner-dialog").fadeIn();
-    });
-});
-
-// Función para cerrar el banner al hacer clic en el botón "Cerrar"
-$("#close-btn").click(function() {
-    $("#banner-dialog").fadeOut();
-});
- // Obtener el botón por su ID
-  const connectButton = document.getElementById("connect-button");
-
-  // Variable para controlar el estado de conexión
-  let isConnected = false;
-
-  // Función para cambiar el texto del botón
-  function updateButtonText() {
-    connectButton.textContent = isConnected ? "Conectado" : "Login Wallet";
-  }
-
-  // Agregar un evento de clic al botón
-  connectButton.addEventListener("click", function () {
-    // Simulación de cambio de estado de conexión (debes implementar la lógica real)
-    isConnected = !isConnected;
-
-    // Actualizar el texto del botón
-    updateButtonText();
-  });
-
-  // Llamar a la función inicial para establecer el texto del botón
-  updateButtonText();
-// Variable para almacenar la cantidad de cada tipo de gallina seleccionada
 // Variable para almacenar la cantidad de gallinas seleccionadas
+let selectedChickenCount = 0;
 
-
-
-  let selectedChickenCount = 0;
-
-  // Función para incrementar y decrementar la cantidad de gallinas en cada card
-  function changeChickenCount(button, increment) {
-    const chickenCountElement = button.parentElement.querySelector(".chicken-selected-count");
-    let currentCount = parseInt(chickenCountElement.innerText);
-    currentCount += increment;
-    if (currentCount < 0) {
-      currentCount = 0;
-    }
-    chickenCountElement.innerText = currentCount;
+// Función para incrementar y decrementar la cantidad de gallinas en cada card
+function changeChickenCount(button, increment) {
+  const chickenCountElement = button.parentElement.querySelector(".chicken-selected-count");
+  let currentCount = parseInt(chickenCountElement.innerText);
+  currentCount += increment;
+  if (currentCount < 0) {
+    currentCount = 0;
   }
+  chickenCountElement.innerText = currentCount;
+  addToMarketCart(button.parentElement.dataset.id, currentCount); // Llamamos a la función para actualizar el cotizador
+}
 
-  // Función para agregar los datos del card y la cantidad al cotizador de mercado
-  function addToMarketCart(chickenId) {
-    const chickenCountElement = document.querySelector(`[data-id="${chickenId}"] .chicken-selected-count`);
-    const selectedCount = parseInt(chickenCountElement.innerText);
-    selectedChickenCount += selectedCount;
+// Función para agregar los datos del card y la cantidad al cotizador de mercado
+function addToMarketCart(chickenId, count) {
+  // Implementa la lógica para agregar los datos del card al cotizador
+  const smallEggPayment = 0.001;
+  const mediumEggPayment = 0.003;
+  const largeEggPayment = 0.005;
 
-    // Actualizar la cantidad de gallinas seleccionada en el cotizador
-    const selectedChickenCountElement = document.getElementById("selected-chicken-count");
-    selectedChickenCountElement.innerText = selectedChickenCount;
+  const totalAmount = (count * smallEggPayment) + (count * mediumEggPayment) + (count * largeEggPayment);
 
-    // Calcular el monto total
-    const smallEggPayment = 0.001;
-    const mediumEggPayment = 0.003;
-    const largeEggPayment = 0.005;
+  // Actualizar la cantidad de gallinas seleccionadas en el cotizador
+  selectedChickenCount += count;
+  const selectedChickenCountElement = document.getElementById("selected-chicken-count");
+  selectedChickenCountElement.innerText = selectedChickenCount;
 
-    const totalAmount = (selectedCount * smallEggPayment) + (selectedCount * mediumEggPayment) + (selectedCount * largeEggPayment);
-
-    // Actualizar el monto total en el cotizador
-    const totalAmountElement = document.getElementById("total-amount");
-    totalAmountElement.innerText = `${totalAmount} BNB`;
-  }
-
+  // Actualizar el monto total en el cotizador
+  const totalAmountElement = document.getElementById("total-amount");
+  totalAmountElement.innerText = `${totalAmount.toFixed(4)} BNB`;
+}
 
 // Conexión con MetaMask y eventos
 window.addEventListener('DOMContentLoaded', () => {
@@ -225,6 +80,16 @@ window.addEventListener('DOMContentLoaded', () => {
   const connectButton = document.getElementById('connect-button');
   connectButton.addEventListener('click', connectToMetaMask);
   // Resto del código omitido por brevedad...
-  
- 
+
+  // Agregar eventos click a los botones de aumentar y el de select
+  const addChickenButtons = document.querySelectorAll('.add-chicken-button');
+  const selectChickenButtons = document.querySelectorAll('.chicken-select-button');
+
+  addChickenButtons.forEach((button) => {
+    button.addEventListener('click', () => changeChickenCount(button, 1));
+  });
+
+  selectChickenButtons.forEach((button) => {
+    button.addEventListener('click', () => addToMarketCart(button.parentElement.dataset.id, parseInt(button.parentElement.querySelector(".chicken-selected-count").innerText)));
+  });
 });
