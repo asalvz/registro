b3aa0195a374329ebef06cfd2fd591cab737f6a9
horacio <114161010+asalvz@users.noreply.github.com>
2023-08-26 11:27:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let web3;
 let userAddress;
+let contractInstance; // Asigna la instancia del contrato aquí (reemplaza 'ContractAbi' y 'ContractAddress')
 
 
 
 
-// Comprobar si web3 está disponible en el navegador
+
 if (typeof window.ethereum !== 'undefined') {
   web3 = new Web3(window.ethereum);
+  contractInstance = new web3.eth.Contract(ContractAbi, ContractAddress);
 } else {
   console.log('No se detectó la billetera MetaMask. Asegúrate de tener instalada la extensión MetaMask en tu navegador.');
 }
@@ -115,7 +117,7 @@ document.body.addEventListener('mouseenter', function (e) {
     }
 
     // Generar el enlace de referido con la dirección como parámetro
-    const referralLink = `https://tusitio.com/registro?ref=${userAddress}`;
+    const referralLink = `${userAddress}`;
 
     // Abreviar el enlace de referido para mostrar solo los primeros 10 caracteres
     const shortenedLink = `${referralLink.substring(0, 30)}...`;
@@ -177,100 +179,7 @@ $("#close-btn").click(function() {
     updateButtonText();
   });
 
-  // Llamar a la función inicial para establecer el texto del botón
-  updateButtonText();
-// Variable para almacenar la cantidad de cada tipo de gallina seleccionada
-// Variable para almacenar la cantidad de gallinas seleccionadas
-
- let selectedChickenCount = 0;
-
-    // Agregar eventos click a los botones de aumentar y disminuir
-    const addChickenButtons = document.querySelectorAll('.add-chicken-button');
-    const removeChickenButtons = document.querySelectorAll('.remove-chicken-button');
-
-    addChickenButtons.forEach((button) => {
-      button.addEventListener('click', () => changeChickenCount(button, 1));
-    });
-
-    removeChickenButtons.forEach((button) => {
-      button.addEventListener('click', () => changeChickenCount(button, -1));
-    });
-
-    // Función para aumentar y disminuir la cantidad de gallinas
-    function changeChickenCount(button, increment) {
-      const chickenCountElement = button.parentElement.parentElement.querySelector(".chicken-selected-count");
-      if (chickenCountElement) {
-        let currentCount = parseInt(chickenCountElement.innerText);
-        currentCount += increment;
-        if (currentCount < 0) {
-          currentCount = 0;
-        }
-        chickenCountElement.innerText = currentCount;
-      }
-    }
-
- let totalEggs = 0;
-let smallEggsCount = 0;
-let mediumEggsCount = 0;
-let largeEggsCount = 0;
-let corralCapacity = 0;
-let chickenPrice = 0.1; // Precio de una gallina (ejemplo, puedes ajustarlo según tus necesidades)
-
-function addToMarketCart(chickenId, increment) {
-  const chickenCountElement = document.querySelector(`[data-id="${chickenId}"] .chicken-selected-count`);
-  const selectedCount = parseInt(chickenCountElement.innerText);
-  selectedChickenCount += increment;
-
-  // Actualizar la cantidad de gallinas seleccionada en el cotizador
-  const selectedChickenCountElement = document.getElementById("selected-chicken-count");
-  selectedChickenCountElement.innerText = selectedChickenCount;
-
-  // Calcular el monto total de las gallinas
-  const totalChickenPrice = selectedChickenCount * chickenPrice;
-
-  // Actualizar el monto total de las gallinas en el cotizador
-  const totalChickenPriceElement = document.getElementById("total-chicken-price");
-  totalChickenPriceElement.innerText = `${totalChickenPrice} BNB`;
-
-  // Actualizar la capacidad de corral (ejemplo: capacidad = gallinas seleccionadas * 3)
-  corralCapacity = selectedChickenCount * 3;
-
-  // Actualizar la capacidad de corral en el cotizador
-  const corralCapacityElement = document.getElementById("corral-capacity");
-  corralCapacityElement.innerText = corralCapacity;
-
-  // Actualizar la cantidad total de huevos con la categoría que le corresponde
-  const smallEggCountElement = document.getElementById("small-egg-count");
-  const mediumEggCountElement = document.getElementById("medium-egg-count");
-  const largeEggCountElement = document.getElementById("large-egg-count");
-
-  smallEggsCount = selectedChickenCount * 2; // Ejemplo: 2 huevos pequeños por gallina
-  mediumEggsCount = selectedChickenCount * 3; // Ejemplo: 3 huevos medianos por gallina
-  largeEggsCount = selectedChickenCount * 4; // Ejemplo: 4 huevos grandes por gallina
-
-  // Actualizar los contadores de huevos en el cotizador
-  smallEggCountElement.innerText = smallEggsCount;
-  mediumEggCountElement.innerText = mediumEggsCount;
-  largeEggCountElement.innerText = largeEggsCount;
-
-  // Calcular el total de huevos
-  totalEggs = smallEggsCount + mediumEggsCount + largeEggsCount;
-
-  // Actualizar el total de huevos en el cotizador
-  const totalEggsElement = document.getElementById("total-eggs");
-  totalEggsElement.innerText = totalEggs;
-
-  // Calcular el monto total
-  const smallEggPayment = 0.001;
-  const mediumEggPayment = 0.003;
-  const largeEggPayment = 0.005;
-  const totalAmount = (smallEggsCount * smallEggPayment) + (mediumEggsCount * mediumEggPayment) + (largeEggsCount * largeEggPayment);
-
-  // Actualizar el monto total en el cotizador
-  const totalAmountElement = document.getElementById("total-amount");
-  totalAmountElement.innerText = `${totalAmount} BNB`;
-}
-
+ 
 function changeButtonColor(button) {
   // Cambiar el color del fondo del botón
   if (button.style.backgroundColor === 'gray') {
@@ -293,14 +202,63 @@ function changeButtonColor(button) {
     expandButton.innerHTML = "Mostrar más";
   }
 }
-function increaseCount() {
-  var chickenCount = document.getElementById("chicken-count");
-  var originalBackgroundColor = chickenCount.style.backgroundColor;
-  chickenCount.style.backgroundColor = "yellow";
-  setTimeout(function() {
-    chickenCount.style.backgroundColor = originalBackgroundColor;
-  }, 300);
+// Función para comprar gallinas
+function buyChickens(amount) {
+  // Lógica para comprar gallinas
+  // Actualizar el estado y balance del contrato
+  // Actualizar el balance del usuario
 }
+
+// Función para ampliar el corral
+function expandCorral() {
+  // Lógica para ampliar el corral
+  // Actualizar el estado y balance del contrato
+}
+
+// Función para reducir el enfriamiento
+function reduceCooldown() {
+  // Lógica para reducir el enfriamiento
+  // Actualizar el estado y balance del contrato
+}
+
+// Función para aumentar el precio de venta
+function increaseSalePrice() {
+  // Lógica para aumentar el precio de venta
+  // Actualizar el estado y balance del contrato
+}
+
+// Función para aumentar la producción
+function increaseProduction() {
+  // Lógica para aumentar la producción
+  // Actualizar el estado y balance del contrato
+}
+
+// Función para generar el enlace de referido
+function generateReferralLink() {
+  // Lógica para generar el enlace de referido
+}
+
+// Evento al hacer clic en el botón para comprar gallinas
+document.getElementById('buy-chickens-button').addEventListener('click', () => {
+  const amount = parseInt(document.getElementById('chicken-amount').value);
+  buyChickens(amount);
+});
+
+// Evento al hacer clic en el botón para ampliar el corral
+document.getElementById('expand-corral-button').addEventListener('click', expandCorral);
+
+// Evento al hacer clic en el botón para reducir el enfriamiento
+document.getElementById('reduce-cooldown-button').addEventListener('click', reduceCooldown);
+
+// Evento al hacer clic en el botón para aumentar el precio de venta
+document.getElementById('increase-price-button').addEventListener('click', increaseSalePrice);
+
+// Evento al hacer clic en el botón para aumentar la producción
+document.getElementById('increase-production-button').addEventListener('click', increaseProduction);
+
+// Evento al hacer clic en el botón para generar el enlace de referido
+document.getElementById('generate-referral-button').addEventListener('click', generateReferralLink);
+
 
 
 
