d11ff275c0d52ca3400e1bf9efe5dd9ff34b30bc
horacio <114161010+asalvz@users.noreply.github.com>
2023-08-30 21:14:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1315,7 +1315,33 @@ sellEggsButton.addEventListener('click', async () => {
     console.error('Error:', error);
   }
 });
- // Asignar la función al botón utilizando una escucha de evento
+	 boostProductivityButton.addEventListener('click', async () => {
+                try {
+                    if (typeof window.ethereum === 'undefined') {
+                        alert('Please install MetaMask to use this feature.');
+                        return;
+                    }
+
+                    await window.ethereum.enable();
+                    const web3 = new Web3(window.ethereum);
+                    const contract = new web3.eth.Contract(contractAbi, contractAddress);
+                    const accounts = await web3.eth.getAccounts();
+                    const userAddress = accounts[0];
+
+                    const cost = await contract.methods.boostProductivityCost().call({ from: userAddress });
+
+                    // Call the boostProductivity function and handle the result
+                    const result = await contract.methods.boostProductivity().send({ from: userAddress, value: cost });
+
+                    console.log('Boosted productivity:', result);
+
+                    // Update the boosts used count
+                    const boostsUsed = await contract.methods.getBoostsUsed(userAddress).call();
+                    boostsUsedElement.textContent = `Boosts used: ${boostsUsed}`;
+                } catch (error) {
+                    console.error(error);
+                }
+            });
 reduceCooldownButton.addEventListener('click', async () => {
     try {
         if (typeof window.ethereum === 'undefined') {
