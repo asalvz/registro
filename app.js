4720b1ee70f03c6004dab661d91da0e11124d46b
horacio <114161010+asalvz@users.noreply.github.com>
2023-09-21 23:01:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1172,37 +1172,22 @@ connectButton.addEventListener('click', async () => {
             alert('Please install MetaMask to use this feature.');
             return;
         }
-
         await window.ethereum.enable();
-
         const web3 = new Web3(window.ethereum);
         const accounts = await web3.eth.getAccounts();
         const userAddress = accounts[0];
-
-        // Actualiza la dirección del usuario en el HTML
         const userAddressElement = document.getElementById('user-address');
         userAddressElement.textContent = `Address: ${userAddress}`;
-
         const bscWeb3 = new Web3('https://bsc-dataseed.binance.org/');
-
         const contract = new bscWeb3.eth.Contract(contractAbi, contractAddress);
-
-        // Obtener la cantidad de "EGG" que posee el usuario
         const eggBalance = await contract.methods.balanceOf(userAddress).call();
         const eggCountElement = document.getElementById('eggCount');
-        eggCountElement.textContent = `Cantidad de EGG: ${eggBalance}`;
-
-        // Obtener la cantidad y tipo de gallinas que posee el usuario
+        eggCountElement.textContent = `EGG Token: ${eggBalance}`;
         const gallinasOwned = await contract.methods.getGallinasOwned(userAddress).call();
-
-        // gallinasOwned es un array que contiene los tipos de gallinas que posee el usuario
-        // Puedes recorrer este array para mostrar los tipos de gallinas en tu interfaz
         const eggTypeElement = document.getElementById('eggType');
-        eggTypeElement.textContent = `Tipo de Gallina: ${gallinasOwned.join(', ')}`;
-
-        // Mostrar la cantidad de gallinas
+        eggTypeElement.textContent = `Type Chicken: ${gallinasOwned.join(', ')}`;
         const gallinasCountElement = document.getElementById('gallinasCount');
-        gallinasCountElement.textContent = `Cantidad de Gallinas: ${gallinasOwned.length}`;
+        gallinasCountElement.textContent = `Balance Owner: ${gallinasOwned.length}`;
 
         connectButton.innerHTML = 'web3 active';
     } catch (error) {
