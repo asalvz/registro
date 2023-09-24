4728d712c8275255eaa5e835c44caaff61152ced
horacio <114161010+asalvz@users.noreply.github.com>
2023-09-24 14:51:18 -0300

    Update game.js

--- game.js
+++ game.js
@@ -11,22 +11,22 @@
             const egg = document.createElement('div');
             egg.classList.add('egg');
             egg.style.left = `${Math.random() * (gameContainer.clientWidth - 30)}px`;
-            egg.style.top = `${gallina.offsetTop + 40}px`; // Posición inicial del huevo cerca de la gallina
+            egg.style.bottom = '0'; // Posición inicial del huevo en la parte inferior
             gameContainer.appendChild(egg);
 
             const fallInterval = setInterval(() => {
-                const eggY = parseInt(egg.style.top) || 0;
+                const eggY = parseInt(egg.style.bottom) || 0;
                 const maxY = gameContainer.clientHeight - egg.clientHeight;
                 if (eggY >= maxY) {
                     clearInterval(fallInterval);
                     gameContainer.removeChild(egg);
                     createEgg(gallina); // Vuelve a crear el huevo
                 } else {
-                    egg.style.top = `${eggY + 2}px`; // Cambia la velocidad de caída ajustando el valor
-                    const gallinaX = gallina.getBoundingClientRect().left + gallina.clientWidth / 2;
-                    const gallinaY = gallina.getBoundingClientRect().top;
+                    egg.style.bottom = `${eggY + 2}px`; // Cambia la velocidad de caída ajustando el valor
+                    const gallinaX = parseInt(getComputedStyle(gallina).left) + gallina.clientWidth / 2;
+                    const gallinaY = parseInt(getComputedStyle(gallina).bottom);
                     const eggX = egg.getBoundingClientRect().left + egg.clientWidth / 2;
-                    const eggY = egg.getBoundingClientRect().top + egg.clientHeight;
+                    const eggY = egg.getBoundingClientRect().bottom;
                     if (Math.abs(gallinaX - eggX) < 25 && eggY >= gallinaY) {
                         clearInterval(fallInterval);
                         gameContainer.removeChild(egg);
