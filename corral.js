519de099c9c629b15a566017f78269260d26defc
horacio <114161010+asalvz@users.noreply.github.com>
2023-09-24 10:39:53 -0300

    Update corral.js

--- corral.js
+++ corral.js
@@ -1,4 +1,4 @@
-   // Función para generar un número aleatorio entre min y max
+  // Función para generar un número aleatorio entre min y max
         function getRandomNumber(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
