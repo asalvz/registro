6bf4bae46684199493d4e2e4b1e67ae0e973498e
horacio <114161010+asalvz@users.noreply.github.com>
2023-09-23 23:59:06 -0300

    Update corral.js

--- corral.js
+++ corral.js
@@ -1,40 +1,62 @@
-// Función para cargar los datos del usuario desde el contrato
-async function cargarDatosUsuario() {
-    let userAddress = null;
+  // Simulación de datos de ranking de referidos y acumulación de Egg
+        const referralRankingData = [
+            "0xAddress1",
+            "0xAddress2",
+            "0xAddress3",
+            "0xAddress4",
+            "0xAddress5"
+        ];
 
-    // Comprobar si MetaMask está disponible
-    if (window.ethereum) {
-        try {
-            // Solicitar acceso a la cuenta del usuario
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const eggAccumulationRankingData = [
+            "0xAddress6",
+            "0xAddress7",
+            "0xAddress8",
+            "0xAddress9",
+            "0xAddress10"
+        ];
 
-            // Obtener la dirección del usuario
-            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-            userAddress = accounts[0];
-        } catch (error) {
-            console.error('Error al solicitar acceso a MetaMask:', error);
-        }
-    }
+        // Función para actualizar el Ranking de Referidos
+        document.getElementById('update-referral-ranking-button').addEventListener('click', () => {
+            const referralListElement = document.getElementById('referral-list');
+            referralListElement.innerHTML = '';
 
-    if (!userAddress) {
-        alert('Por favor, conéctese a MetaMask para ver sus datos.');
-        return;
-    }
+            for (let i = 0; i < referralRankingData.length; i++) {
+                const row = document.createElement('tr');
+                const rankCell = document.createElement('td');
+                rankCell.textContent = `Puesto ${i + 1}`;
+                const addressCell = document.createElement('td');
+                addressCell.textContent = referralRankingData[i];
 
-    // Obtener los estados del contrato para el usuario
-    const gallinas = await contract.getGallinasOwned(userAddress);
-    const referido = await contract.getReferido(userAddress);
-    const eggBalance = await contract.getEggBalance(userAddress);
-    const upgrades = await contract.getUpgrades(userAddress);
+                row.appendChild(rankCell);
+                row.appendChild(addressCell);
+                referralListElement.appendChild(row);
+            }
+        });
 
-    // Mostrar los datos en la interfaz
-    document.getElementById('gallinas').textContent = `Gallinas: ${gallinas.length}`;
-    document.getElementById('referido').textContent = `Referido: ${referido}`;
-    document.getElementById('eggBalance').textContent = `Egg: ${eggBalance}`;
-    document.getElementById('upgrades').textContent = `Upgrades: ${upgrades.join(', ')}`;
-}
+        // Función para actualizar el Ranking de Acumulación de Egg
+        document.getElementById('update-egg-accumulation-ranking-button').addEventListener('click', () => {
+            const eggAccumulationListElement = document.getElementById('egg-accumulation-list');
+            eggAccumulationListElement.innerHTML = '';
 
-// Llamar a la función para cargar los datos del usuario al cargar la página
-window.addEventListener('load', cargarDatosUsuario);
+            for (let i = 0; i < eggAccumulationRankingData.length; i++) {
+                const row = document.createElement('tr');
+                const rankCell = document.createElement('td');
+                rankCell.textContent = `Puesto ${i + 1}`;
+                const addressCell = document.createElement('td');
+                addressCell.textContent = eggAccumulationRankingData[i];
 
-// Resto del código
+                row.appendChild(rankCell);
+                row.appendChild(addressCell);
+                eggAccumulationListElement.appendChild(row);
+            }
+        });
+
+        // Simulación de datos del usuario
+        const userAddressData = "0xUserAddress";
+        const userEggAmountData = 1000;
+
+        // Actualiza los datos del usuario al cargar la página
+        window.addEventListener('load', () => {
+            document.getElementById('real-user-address').textContent = userAddressData;
+            document.getElementById('real-egg-amount').textContent = userEggAmountData;
+        });
